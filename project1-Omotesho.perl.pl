92d21d03dd71e1488f19cc6d4d91d62279c6be3c
Horlardmaygee <89870019+Horlardmaygee@users.noreply.github.com>
2021-10-12 09:10:36 +0100

    Add files via upload

---
+++ project1-Omotesho.perl.pl
@@ -0,0 +1,2 @@
+
+print "Name ","- ","Omotesho Quadri\n","Email " ,"- " ,"omoteshoquadri@gmail.com\n","Discord_username " ,"- ","@","Quadri\n","Biostack ","- ","Genomic Medicine ","& ","Drug discovery";
