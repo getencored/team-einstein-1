521a89b994c9640a0cd8df2b44185959c9209324
Horlardmaygee <89870019+Horlardmaygee@users.noreply.github.com>
2021-10-12 16:56:06 +0100

    Update project1-Omotesho.perl.pl

--- project1-Omotesho.perl.pl
+++ project1-Omotesho.perl.pl
@@ -1,2 +1,1 @@
-
 print "Name ","- ","Omotesho Quadri\n","Email " ,"- " ,"omoteshoquadri@gmail.com\n","Discord_username " ,"- ","@","Quadri\n","Biostack ","- ","Genomic Medicine ","& ","Drug discovery";
